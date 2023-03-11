cc48efc11ec46d21d1e7a048affece3d43af3af7
SK SAHIL RAHAMAN <122079533+sahilrahaman29@users.noreply.github.com>
2023-03-10 22:49:56 -0800

    New java files upload

---
+++ Assignment.java
@@ -0,0 +1,22 @@
+class Assignment { 
+    public static void main(String []args) 
+    {
+        int m1, m2, m3, m4;
+        m1=m2=m3=m4=10;
+        System.out.println(m1 + " " 
+        + m2 + " " + m3 + " " + m4);
+        int a =1;
+        a +=5;
+        int b=2;
+        b -=5;
+        int c;
+        c = 360;
+        c /=5;
+        int d = 225;
+        d *=10;
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(b);
+    }    
+}
